13321128ddc43a330cb452736323fed877fc2692
jpmitchellpierson <jpmitchellpierson@gmail.com>
2017-07-05 14:56:07 -0700

    actions and reducers setup

---
+++ reducers.js
@@ -0,0 +1,60 @@
+// import actions to pass to reducers
+import { VisibilityFilters, ADD_TODO, TOGGLE_TODO } from './actions';
+import { combineReducers } from 'redux';
+
+const { SHOW_ALL } = VisibilityFilters
+
+// set initial state of todo app with no filter and blank todos
+const initialState = {
+  visibilityFilter: VisibilityFilters.SHOW_ALL,
+  todos: []
+};
+
+
+function todos(state = [], action) {
+  switch (action.type) {
+    case ADD_TODO:
+      return [
+        ...state,
+        {
+          text: action.text,
+          completed: false
+        }
+      ]
+    case TOGGLE_TODO:
+      return state.map((todo, index) => {
+        if (index === action.index) {
+          return Object.assign({}, todo, {
+            completed: !todo.completed
+          })
+        }
+        return todo
+      })
+    default:
+      return state
+  }
+}
+
+function visibilityFilter(state = SHOW_ALL, action) {
+  switch (action.type) {
+    case SET_VISIBILITY_FILTER:
+      return action.filter
+    default:
+      return state
+  }
+}
+
+const todoApp = combineReducers({
+  visibilityFilter,
+  todos
+})
+// equivalent to below
+
+// function todoApp(state = {}, action) {
+//   return {
+//     visibilityFilter: visibilityFilter(state.visibilityFilter, action),
+//     todos: todos(state.todos, action)
+//   }
+// }
+
+export default todoApp;
